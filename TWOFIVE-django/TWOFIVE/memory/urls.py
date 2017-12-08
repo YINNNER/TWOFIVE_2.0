68fd73264b2d2b8f95cb6c905ab833f54c7325b2
Panda98 <810650217@qq.com>
2017-12-09 00:09:22 +0800

    修改

--- TWOFIVE-django/TWOFIVE/memory/urls.py
+++ TWOFIVE-django/TWOFIVE/memory/urls.py
@@ -1,18 +1,18 @@
-from django.conf.urls import url,include
+# -*- coding: utf-8 -*-
+from django.conf.urls import url
 from django.views import static
 from TWOFIVE.settings import MEDIA_ROOT
 from . import views
 
 urlpatterns = [
-    url(r'^homepage/$', views.homepage,name='homepage'),
-    url(r'^login/$', views.login_view,name='login_view'),
-    url(r'^login/$',views.register,name='register'),
-    url(r'^writing/$',views.writing,name='writing'),
-    url(r'^publish/$',views.publish,name='publish'),
-    url(r'^browsing/$',views.browsing,name='browsing'),
-    url(r'^album/$',views.album,name='album'),
-    url(r'^settings/$',views.settings,name='settings'),
-    url(r'^ajax_name/$',views.ajax_name,name='ajax_name'),
+    url(r'^homepage/$', views.homepage,name='homepage'), # 主頁url
+    url(r'^login/$', views.login_view,name='login_view'), # 登錄的處理url
+    url(r'^login/$',views.register,name='register'),# 注冊的處理url
+    url(r'^writing/$',views.writing,name='writing'), # 寫文章界面
+    url(r'^publish/$',views.publish,name='publish'), # 發佈按鈕的url
+    url(r'^browsing/$',views.browsing,name='browsing'), # 推薦界面的url
+    url(r'^album/$',views.album,name='album'), # 相冊界面的url
+    url(r'^settings/$',views.settings,name='settings'), # 設置界面的url
     url(r'^media/(?P<path>.*)$',static.serve,{'document_root':MEDIA_ROOT,}),
-    url(r'^user_setting/$',views.user_setting,name="user_setting"),
+    url(r'^user_setting/$',views.user_setting,name="user_setting"), # 設置的處理url
 ]
--- TWOFIVE-django/TWOFIVE/memory/views.py
+++ TWOFIVE-django/TWOFIVE/memory/views.py
@@ -13,7 +13,7 @@ from PIL import Image,ImageFile
 from TWOFIVE.settings import MEDIA_ROOT,MEDIA_URL
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 # Create your views here.
-
+# 注冊
 def register(request):
     redirect_to=request.POST.get('next',request.GET.get('next',''))
     if (request.method == 'POST'):
@@ -26,11 +26,12 @@ def register(request):
         form = RegisterForm()
     return form
 
+# 主頁
 @login_required
 def homepage(request):
     return render(request,'homepage.html')
 
-
+# 登錄
 def login_view(request):
     request.session.set_expiry(0)
     if request.user.is_authenticated():
@@ -62,11 +63,12 @@ def login_view(request):
         regist_form=register(request)
         return render(request,'registration/login.html',context={'login_form':login_form,'form':regist_form})
 
+# 寫文章頁面
 @login_required
 def writing(request):
     return render(request,'writing.html')
 
-# 发布
+# 发布文章
 @login_required
 def publish(request):
     if request.is_ajax():
@@ -74,36 +76,38 @@ def publish(request):
     is_success={'is_success':'success'}
     return JsonResponse(is_success)
 
+# 推薦頁面
 @login_required
 def browsing(request):
     return render(request,'browsing.html')
 
+# 相冊頁面
 @login_required
 def album(request):
     return render(request,'album.html')
 
+# 設置頁面
 def settings(request):
     return render(request,'settings.html')
 
+# 注銷登陸
 def logout_view(request):
     logout(request)
     return render_to_response('registration/login.html')
 
-# 传个人简介和用户名给homepage
-def ajax_name(request):
-    username={'name':'ha'}
-    # return JsonResponse(username)
-    return HttpResponse(json.dumps(username),content_type='application/json')
-
+# 獲得用戶上傳的頭像照片，並進行裁剪
 def upload_file(request,filename):
     if request.method =='POST':
-        myFile=request.FILES.get(filename,None) #获取用户上传文件，若没有则为None
+        myFile=request.FILES.get(filename,None) # 获取用户上传圖片，若没有则为None
         if not myFile:
             return False
+        # 將用戶上傳的圖片存到服務器
         pic_url=os.path.join(MEDIA_ROOT,request.user.username+'_por_pic.png')
         destination=open(pic_url,'wb+')
         for chunk in myFile.chunks():
             destination.write(chunk)
+
+        #用戶圖片裁剪成正方形
         img=Image.open(pic_url)
         width=img.size[0]
         height=img.size[1]
@@ -116,16 +120,18 @@ def upload_file(request,filename):
         destination.close()
         return cut_url
 
-
+# 用戶設置
 def user_setting(request):
     if request.method=='POST':
         portrait = request.FILES.get('portrait')
         portrait_url='not_portrait'
         if portrait != None:
-            portrait_url=upload_file(request,'portrait')
+            portrait_url=upload_file(request,'portrait') # 獲得用戶頭像的url
+        # 若portrait_url為空，説明設置失敗
         if not portrait_url:
             is_success={'is_success':'portrait_failure'}
             return JsonResponse(is_success)
+
         # 修改昵称
         is_nickname=False
         if request.POST.get('nickname')!=request.user.nickname and request.POST.get('title')!='':
@@ -138,6 +144,7 @@ def user_setting(request):
             request.user.title=request.POST.get('title')
             is_title=True
 
+        # 判斷是否修改成功，成功則is_success賦值為success，若用戶沒有修改，則is_success賦值為not_change,返回給前端
         if is_nickname or is_title:
             request.user.save()
             is_success={'is_success':'success'}
